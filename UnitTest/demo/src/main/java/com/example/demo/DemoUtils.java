1d8318519a6adc568cc1f4c7fed3b834eadb0cdb
Andrey Dobrikov <andrey.dobrikov@nice.com>
2022-08-14 08:45:01 +0300

    all code

--- UnitTest/demo/src/main/java/com/example/demo/DemoUtils.java
+++ UnitTest/demo/src/main/java/com/example/demo/DemoUtils.java
@@ -12,4 +12,17 @@ public class DemoUtils {
         return a > b;
     }
 
+    public String isGreaterThan0(int a) throws Exception {
+        if(a < 0) {
+            throw new MyException("Value should be greater or equals 0");
+        }
+        return "Value is greater or equals 0";
+    }
+
+    public void checkTimeout() throws InterruptedException {
+        System.out.println("Going to sleep");
+        Thread.sleep(2);
+        System.out.println("Wake up!");
+    }
+
 }
--- UnitTest/demo/src/test/java/com/example/demo/MyTest.java
+++ UnitTest/demo/src/test/java/com/example/demo/MyTest.java
@@ -2,9 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
 public class MyTest {
 
     private DemoUtils demoUtils;
@@ -35,10 +38,22 @@ public class MyTest {
 
     @Test
     @DisplayName("True or False")
+    @Order(1)
     public void testTrueAndFalse() {
         assertTrue(demoUtils.isGrater(10, 2), "Should be greater");
         assertFalse(demoUtils.isGrater(2, 10), "Should be lesser");
     }
 
+    @Test
+    @DisplayName("Throws and doesn't")
+    public void testThrows() {
+        assertThrows(MyException.class, () -> demoUtils.isGreaterThan0(-1));
+        assertDoesNotThrow(() -> demoUtils.isGreaterThan0(1));
+    }
 
+    @Test
+    @DisplayName("Test not oversleeps")
+    public void testTimeout() {
+        assertTimeoutPreemptively(Duration.ofSeconds(3), () -> demoUtils.checkTimeout());
+    }
 }
